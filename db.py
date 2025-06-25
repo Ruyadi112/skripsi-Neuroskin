287af21dfad4eb8ba7103ff1ddf2fb2771dab7d9
Ruyadi <93931512+Ruyadi112@users.noreply.github.com>
2025-06-25 15:42:39 +0700

    Update db.py

--- db.py
+++ db.py
@@ -1,9 +1,10 @@
+import os
 import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host="sql12.freesqldatabase.com",
-        user="sql12786702", 
-        password="1VmzvnIlgu",
-        database="sql12786702" 
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASS"),
+        database=os.getenv("DB_NAME")
     )
